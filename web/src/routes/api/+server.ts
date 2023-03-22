770eab515042832de8d67f7bcc7dd04551cceb63
orosmatthew <orosmatthew@pm.me>
2023-03-21 23:53:12 -0400

    Add counter table in db

--- web/src/routes/api/+server.ts
+++ web/src/routes/api/+server.ts
@@ -1,7 +1,10 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const POST = (async ({ request }) => {
   let data = await request.json();
-  console.log(data);
+  await prisma.counter.create({ data: { num: data.test } });
   return json({ status: 'success' });
 }) satisfies RequestHandler;
