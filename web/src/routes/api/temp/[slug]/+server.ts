ee854f717a27512ec8abbb78c75f33c75a11275c
orosmatthew <orosmatthew@pm.me>
2023-03-23 10:14:12 -0400

    Refactor temp GET

--- web/src/routes/api/temp/[slug]/+server.ts
+++ web/src/routes/api/temp/[slug]/+server.ts
@@ -1,4 +1,4 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { PrismaClient } from '@prisma/client';
 import type { TempData } from '../types';
@@ -8,9 +8,12 @@ const prisma = new PrismaClient();
 const MS_PER_MINUTE = 60000;
 
 export const GET = (async ({ params }) => {
-  let num = parseInt(params.slug);
+  let minutes = parseInt(params.slug);
+  if (isNaN(minutes)) {
+    throw error(400, 'invalid');
+  }
   let data = await prisma.temp.findMany({
-    where: { createdAt: { gte: new Date(new Date().valueOf() - num * MS_PER_MINUTE) } }
+    where: { createdAt: { gte: new Date(new Date().valueOf() - minutes * MS_PER_MINUTE) } }
   });
   let tempData: TempData = { temp: data };
   return json(tempData);
